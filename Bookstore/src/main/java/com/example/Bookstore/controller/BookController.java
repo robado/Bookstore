e5997ca0bf8a4da925a71883d78088a726ffaf15
Robert Ridal <ridal.robert@gmail.com>
2018-02-07 20:38:21 +0200

    Rest tehty

--- Bookstore/src/main/java/com/example/Bookstore/controller/BookController.java
+++ Bookstore/src/main/java/com/example/Bookstore/controller/BookController.java
@@ -8,16 +8,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class BookController {
     @Autowired
     private BookRepository repository;
 
+    @RequestMapping("/index")
+    public String home() {
+        return "index";
+    }
+
     @RequestMapping(value="/booklist")
     public String studentList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    //RESTful
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {
+        return (List<Book>) repository.findAll();
+    }
+
+    @RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {
+        return repository.findOne(bookId);
     }
 
     @RequestMapping(value = "/add")
